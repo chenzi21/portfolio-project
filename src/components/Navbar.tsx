6431bc2b7023ebf66ce44e8f03bf6b46160250b4
Chen <chenzg12345@gmail.com>
2023-04-05 16:12:29 +0300

    - implmented hamburger menu in sm-window breakpoint (768px) - made app more responsive by using dynamic values

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import styles from "../styles/navbar.module.scss";
 import { inter } from "@/libs/fonts";
 
@@ -18,8 +18,10 @@ export const navigations: { title: string; url: string }[] = [
 ]
 
 export default function Navbar() {
+    const [checked, setChecked] = useState<boolean>(false);
+
     const navigationsListItems = useMemo(() => navigations.map(({ title, url }) => (
-        <li className={inter.className} key={title}>
+        <li className={inter.className} key={title} onClick={() => setChecked(false)}>
             <a href={url} className={styles["nav-link"]}>
                 {title}
             </a>
@@ -28,8 +30,21 @@ export default function Navbar() {
 
     return (
         <nav className={styles.navbar}>
-            <ul className={styles["nav-list"]}>
+            <input
+                className={styles.checkbox}
+                id="menu-toggle"
+                type="checkbox"
+                hidden
+                checked={checked}
+                aria-checked={checked}
+                aria-label="Navigation
+                Bar Controller"/>
+            <label className={styles["checkbox-label"]} htmlFor="menu-toggle" onClick={() => setChecked((prev) => !prev)}>
+                <div className={styles["hamburger"]}></div>
+            </label>
+            <ul className={styles["nav-list"]} aria-expanded={checked} >
                 {navigationsListItems}
             </ul>
-        </nav>)
+        </nav>
+    )
 }
--- src/components/intro/IntroText.tsx
+++ src/components/intro/IntroText.tsx
@@ -2,17 +2,14 @@ import styles from "../../styles/intro/introText.module.scss";
 import { inter } from "../../libs/fonts";
 
 export default function IntroText() {
-    return (<div className={styles.wrapper}>
+    return (
         <header className={styles.header}>
-            <h1 className={`${styles.text} ${inter.className}`}>
-                Hello, I'm <span>Chen Zadik</span>,
+            <h1 className={inter.className}>
+                Hello, I'm <span>Chen Zadik</span>, a {`<Full Stack Developer>`}.
             </h1>
-            <h1 className={`${styles.text} ${inter.className}`}>
-                a {`<Full Stack Developer>`}.
-            </h1>
+            <p className={styles["sub-header"]}>
+                I'm passionate about learning new technologies as well as writing testable, reusable and clean code.
+            </p>
         </header>
-        <p className={styles["sub-header"]}>
-            I'm passionate about learning new technologies as well as writing testable, reusable and clean code.
-        </p>
-    </div>)
+    )
 }
